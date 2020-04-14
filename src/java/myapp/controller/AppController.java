14741944f92c30ad5062aa2ff81009fb0dd9b9f1
ManuelGhion <manuel.ghion@hotmail.it>
2020-04-14 14:06:02 +0200

    LOGIN

--- src/java/myapp/controller/AppController.java
+++ src/java/myapp/controller/AppController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
-import myapp.model.Employee;
-import myapp.service.EmployeeService;
+import myapp.model.Utenti;
+import myapp.service.UtentiService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.ModelAttribute;
  
@@ -27,49 +27,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class AppController {
  
     @Autowired
-    EmployeeService service;
+    UtentiService utente;
      
  
     /*
      * This method will list all existing employees.
      */
-    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-    public String listEmployees(ModelMap model) {
- 
-        List<Employee> employees = service.findAllEmployees();
-        model.addAttribute("tutti", employees);
-        return "impiegatil";
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+        public String index(ModelMap model) {
+        model.addAttribute("utente", new Utenti());
+        return "login";
     }
- 
-    @RequestMapping(value = "/add" , method = RequestMethod.GET)
-    public String newEmployee(ModelMap model) {
-        Employee employee = new Employee();
-        model.addAttribute("employee", employee);
-        return "add";
-    }
-
     
-    //For add and update person both
-    @RequestMapping(value= "/add", method = RequestMethod.POST)
-    public String addEmpl(@ModelAttribute("employee") Employee e){
-		
-	if(e.getId() == 0) this.service.saveEmployee(e);
-        else this.service.updateEmployee(e);
-        
-	return "redirect:/";
-		
-    }
-	
-    @RequestMapping("/remove/{id}")
-    public String removeEmpl(@PathVariable("id") int id){
-	this.service.deleteEmployee(id);
-        return "redirect:/";
-    }
- 
-    @RequestMapping("/edit/{id}")
-    public String editPerson(@PathVariable("id") int id, ModelMap model){
-        model.addAttribute("employee", this.service.findById(id));
-        return "edit";
-    }
-	
 }
